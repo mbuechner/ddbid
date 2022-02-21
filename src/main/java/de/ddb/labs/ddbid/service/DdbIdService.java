3558707a3573802c234a90fc91b9a048cb610c82
Michael Büchner <m.buechner@dnb.de>
2022-02-21 21:55:00 +0100

    Added valiable timestamp from JSON array

--- src/main/java/de/ddb/labs/ddbid/service/DdbIdService.java
+++ src/main/java/de/ddb/labs/ddbid/service/DdbIdService.java
@@ -18,33 +18,34 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @Slf4j
 @Service
 public class DdbIdService {
-    
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    
+
     private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
-    
+
     public Page<DdbId> getDdbIds(PagingRequest pagingRequest) {
-        
+
         String status = null;
         final List<String> s = Arrays.stream(Status.values()).map(Enum::name).collect(Collectors.toList());
         if (s.contains(pagingRequest.getStatus())) {
             status = pagingRequest.getStatus();
         }
-        
+
         int totalCount;
         if (status != null) {
             totalCount = jdbcTemplate.queryForObject("SELECT count(*) FROM main.\"data\" WHERE status='" + status + "'", Integer.class);
         } else {
             totalCount = jdbcTemplate.queryForObject("SELECT count(*) FROM main.\"data\"", Integer.class);
         }
-        
+
         final StringBuilder query = new StringBuilder("SELECT * FROM main.\"data\" ");
 
         // WHERE (Search)
@@ -69,7 +70,7 @@ public class DdbIdService {
             query.append("WHERE status=? ");
             whereValues.add(status);
         }
-        
+
         final String filteredCountQuery = query.toString().replaceFirst("\\*", "count(*)");
         final int filteredCount = jdbcTemplate.queryForObject(filteredCountQuery, Integer.class, whereValues.toArray());
 
@@ -104,28 +105,33 @@ public class DdbIdService {
         final List<String> values = new ArrayList<>();
         values.addAll(whereValues);
         values.addAll(limitValues);
-        
+
         List<DdbId> ddbIds;
         if (values.isEmpty()) {
             ddbIds = jdbcTemplate.query(query.toString(), new BeanPropertyRowMapper(DdbId.class));
         } else {
             ddbIds = jdbcTemplate.query(query.toString(), new BeanPropertyRowMapper(DdbId.class), values.toArray());
         }
-        
+
         final Page<DdbId> page = new Page<>(ddbIds);
         page.setRecordsFiltered(filteredCount);
         page.setRecordsTotal(totalCount);
         page.setDraw(pagingRequest.getDraw());
-        
+
         return page;
     }
-    
+
     public Map<Timestamp, String> getTimestamps() {
-        final Timestamp[] ts = jdbcTemplate.queryForObject("SELECT DISTINCT \"timestamp\" FROM main.\"data\"", Timestamp[].class);
-        final Map<Timestamp, String> m = new HashMap<>();
-        for (Timestamp t : ts) {
-            m.put(t, sdf.format(t));
+        try {
+            final Timestamp[] ts = jdbcTemplate.queryForObject("SELECT DISTINCT \"timestamp\" FROM main.\"data\"", Timestamp[].class);
+            final Map<Timestamp, String> m = new HashMap<>();
+            for (Timestamp t : ts) {
+                m.put(t, sdf.format(t));
+            }
+            return m;
+        } catch (EmptyResultDataAccessException e) {
+            log.debug("No record found in database for timestamp", e);
+            return null;
         }
-        return m;
     }
 }
