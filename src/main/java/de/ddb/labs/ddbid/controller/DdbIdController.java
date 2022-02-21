727650f4ce6a024f415bdcb306b804fd8eb88589
Michael Büchner <m.buechner@dnb.de>
2022-02-21 17:31:40 +0100

    Stand 20220221

--- src/main/java/de/ddb/labs/ddbid/controller/DdbIdController.java
+++ src/main/java/de/ddb/labs/ddbid/controller/DdbIdController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping({ "/item" })
+@RequestMapping({ "/item.html" })
 public class DdbIdController {
 
     @GetMapping
