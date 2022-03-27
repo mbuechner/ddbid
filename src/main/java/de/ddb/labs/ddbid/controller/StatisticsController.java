002e7e4d9b95006bd83ca6c4ed5a4fedda8582d2
Michael Büchner <m.buechner@dnb.de>
2022-03-27 19:10:09 +0200

    IS NULL-SQL bug fixed

--- src/main/java/de/ddb/labs/ddbid/controller/StatisticsController.java
+++ src/main/java/de/ddb/labs/ddbid/controller/StatisticsController.java
@@ -46,9 +46,8 @@ public class StatisticsController {
             + "GROUP BY provider_id\n"
             + "ORDER BY count(id) DESC;";
 
-    private final static String MISSING_BY_SECTOR = "SELECT sector_fct, count(id) AS MISSING FROM main.item \n"
-            + "WHERE status = 'MISSING'\n"
-            + "AND status != null\n"
+    private final static String MISSING_BY_SECTOR = "SELECT sector_fct, count(id) AS MISSING FROM main.item\n"
+            + "WHERE status = 'MISSING' AND sector_fct IS NOT NULL\n"
             + "GROUP BY sector_fct\n"
             + "ORDER BY count(id) DESC;";
 
