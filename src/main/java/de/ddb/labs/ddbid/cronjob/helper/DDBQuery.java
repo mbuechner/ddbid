6a94e49292d4af52fd8b2107fc2c7da06c26c1fd
Michael Büchner <m.buechner@dnb.de>
2022-09-20 11:29:02 +0200

    Added back DownloadRestController; upgrade of all libraries

--- src/main/java/de/ddb/labs/ddbid/cronjob/helper/DDBQuery.java
+++ src/main/java/de/ddb/labs/ddbid/cronjob/helper/DDBQuery.java
@@ -24,7 +24,6 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
